1d5d718c4b8f7c6a104a1dde573273d025fda9d9
Zhao, Xin <xin.zhao@pnnl.gov>
2024-02-22 15:10:09 -0500

    Adding files
    1. adding a temp folder with some data
    2. pull AR6 variables and definitions
    3. adding functions to convert yaml
    
    Will need to make changes in  AR6_Var_AFOLU_all.csv and update tiers

---
+++ temp/AgLU_Variables.R
@@ -0,0 +1,82 @@
+library(dplyr)
+library(tidyr)
+
+# Load AR6 definitions
+readxl::read_excel("temp/IPCC_AR6_WG3_Global_sectoral_Pathways_scenario_template_v2.1.xlsx",
+                   sheet = "variable_definitions_Full") %>%
+  select(Category, Variable, Unit, Definition, Core, AFOLU) -> AR6_Var
+
+
+AR6_Var %>%
+  # keep only AFOLU variables
+  filter(!is.na(AFOLU)) %>%
+  # remove emission variables
+  filter(Category != "Emissions") ->
+  AR6_Var_AFOLU_original
+
+## Adding AFOLU related food variables ----
+AR6_Var %>%
+  filter(is.na(AFOLU)) %>%
+  filter(grepl("Food", Variable)) ->
+  AR6_Var_AFOLU_add_food
+
+## Adding AFOLU related water variables ----
+AR6_Var %>%
+  filter(Category == "Water") %>%
+  filter(grepl("Irrigation|Livestock", Variable)) ->
+  AR6_Var_AFOLU_add_water
+
+## Adding AFOLU related price variables ----
+AR6_Var %>%
+  filter(grepl("Price", Variable)) %>%
+  # Keep only biomass or ag
+  filter(grepl("Primary Energy\\|Biomass|Ag", Variable)) ->
+  AR6_Var_AFOLU_add_price
+
+## Adding AFOLU related labor variables ----
+AR6_Var %>%
+  filter(grepl("Employment", Variable)) %>%
+  # Keep only biomass or ag
+  filter(grepl("Ag", Variable)) ->
+  AR6_Var_AFOLU_add_labor
+
+
+# Bind all----
+AR6_Var_AFOLU_original %>%
+  bind_rows(AR6_Var_AFOLU_add_food) %>%
+  bind_rows(AR6_Var_AFOLU_add_water) %>%
+  bind_rows(AR6_Var_AFOLU_add_price) %>%
+  bind_rows(AR6_Var_AFOLU_add_labor) ->
+  AR6_Var_AFOLU_all
+
+AR6_Var_AFOLU_all %>% readr::write_csv("temp/AR6_Var_AFOLU_all.csv")
+
+
+
+DF_To_yamlList <- function(.df){
+
+  #assert columns in df include key ones
+  assertthat::assert_that(is.data.frame(.df))
+  assertthat::assert_that(all(c("Variable", "Description", "Unit", "Tier") %in% names(.df)))
+  assertthat::assert_that(is.integer(.df$Tier), msg = "Column is not of integer type; please convert it.")
+
+  lapply(1:nrow(.df), function(x){
+
+    .df %>% dplyr::slice(x) %>%
+      select(Variable, Description, Unit, Tier) %>%
+      group_by(Variable) %>%
+      nest() %>% ungroup() %>%
+      mutate(data = list(Variable = as.list(data %>% purrr::pluck(1) ) )) -> .df2
+
+    names(.df2$data) <- .df2$Variable
+
+    .df2$data
+  }
+  )
+}
+
+AR6_Var_AFOLU_all %>%
+  mutate(Tier = as.integer(Core), Description = Definition) %>%
+  DF_To_yamlList %>%
+  yaml::write_yaml("temp/test.yaml")
+
